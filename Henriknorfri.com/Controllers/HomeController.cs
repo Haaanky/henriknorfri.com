69f4c87b6aee2f2adae3249a0dd707e895b7c459
Haaanky <Haaanky@gmail.com>
2019-05-13 16:53:46 +0200

    added some text, typed.js and stuff

--- Henriknorfri.com/Controllers/HomeController.cs
+++ Henriknorfri.com/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace Henriknorfri.com.Controllers
         {
             return View();
         }
-
+        [Route("Privacy")]
         public IActionResult Privacy()
         {
             return View();
